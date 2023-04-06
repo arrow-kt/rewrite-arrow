6a7411ebaac79ec9cbd5308d13b806d6496b2088
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-04-06 07:45:20 +0200

    Revert "Merge pull request #14 from arrow-kt/openrewrite-library-plugin"
    This reverts commit a8bd19be896b6e9986425d49fa822a74230a45a4, reversing
    changes made to 90605becee90049a4a2bca01f7146596fdb207c4.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,25 @@
+import java.lang.System.getenv
+
 @Suppress("DSL_SCOPE_VIOLATION") plugins {
-  id("org.openrewrite.build.recipe-library") version "1.9.3"
+  `java-library`
+  `maven-publish`
+  signing
+  alias(libs.plugins.nexus)
 }
 
 group = "io.arrow-kt"
-description = "A rewrite module automating Arrow refactorings from 1.x.x to 1.2.x/2.x.x."
 
-// Doesn't affect version being inject by plugin
-//rewriteRecipe {
-//  version = requireNotNull(libs.rewrite.core.get().version)
-//}
+repositories {
+  mavenCentral()
+}
+
+tasks.named<JavaCompile>("compileJava") {
+  options.release.set(8)
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
 
 dependencies {
   annotationProcessor(libs.lombok)
@@ -22,4 +33,60 @@ dependencies {
   testRuntimeOnly(libs.arrow.core)
   testRuntimeOnly(libs.junit.jupiter.engine)
   testRuntimeOnly(libs.lombok)
+}
+
+java {
+  withJavadocJar()
+  withSourcesJar()
+}
+
+nexusPublishing {
+  repositories {
+    sonatype {
+      username.set(getenv("OSS_USER"))
+      password.set(getenv("OSS_TOKEN"))
+      stagingProfileId.set(getenv("OSS_STAGING_PROFILE_ID"))
+    }
+  }
+}
+
+signing {
+  useInMemoryPgpKeys(
+    getenv("SIGNING_KEY_ID"),
+    getenv("SIGNING_KEY"),
+    getenv("SIGNING_KEY_PASSPHRASE")
+  )
+  sign(project.extensions.getByName<PublishingExtension>("publishing").publications)
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+      artifactId = "rewrite-arrow"
+      from(components["java"])
+      pom {
+        name.set("Rewrite Arrow")
+        description.set("OpenRewrite recipes for Arrow 1.2.0-RC / 2.0.0")
+        url.set("https://github.com/arrow-kt/rewrite-arrow")
+        licenses {
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+        developers {
+          developer {
+            id.set("nomisRev")
+            name.set("Simon Vergauwen")
+            email.set("vergauwen.simon@gmail.com")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/arrow-kt/rewrite-arrow.git")
+          developerConnection.set("scm:git:ssh://github.com/arrow-kt/rewrite-arrow.git")
+          url.set("https://github.com/arrow-kt/rewrite-arrow")
+        }
+      }
+    }
+  }
 }
